6c0b93cb7bf62a987cb34bea6cfc6406770368c4
Lukas Rendon <lukas.rendon@unp.gov.co>
2024-12-12 18:42:51 -0500

    Ajustes

--- src/components/forms/entorno/componenteActividad.tsx
+++ src/components/forms/entorno/componenteActividad.tsx
@@ -1,51 +1,76 @@
 import React, { ChangeEvent } from "react";
-import { Row, Col, Form, FormGroup, FormLabel, FormControl } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  FormLabel,
+  FormControl,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { FaCalendarAlt, FaMapMarkerAlt } from "react-icons/fa";
 import { SubtituloForm } from "eco-unp/ui";
 
 interface ComponenteActividadProps {
-    formData: {
-        inspeccionFechaActividad: string;
-        actividadDistancia: string;
-    };
-    handleChange: (e: ChangeEvent<any>) => void;
+  formData: {
+    inspeccionFechaActividad: string;
+    actividadDistancia: string;
+  };
+  handleChange: (e: ChangeEvent<any>) => void;
 }
 
-const ComponenteActividad: React.FC<ComponenteActividadProps> = ({ formData, handleChange }) => (
-    <>
-        <Row>
-            <Col md={6} className="mb-3">
-            <SubtituloForm subtitulo="Fecha de realización de la inspección" icon={FaCalendarAlt} />
+const ComponenteActividad: React.FC<ComponenteActividadProps> = ({
+  formData,
+  handleChange,
+}) => (
+  <>
+    <Row>
+      <Col md={6} className="mb-3">
+        <SubtituloForm
+          subtitulo="Fecha de realización de la inspección"
+          icon={FaCalendarAlt}
+        />
 
-                <FormGroup>
-                    <FormLabel>Fecha</FormLabel>
-                    <FormControl
-                        type="date"
-                        name="inspeccionFechaActividad"
-                        value={formData.inspeccionFechaActividad}
-                        onChange={handleChange}
-                    />
-                </FormGroup>
-            </Col>
-            <Col md={6} className="mb-3">
-            <SubtituloForm subtitulo="Distancia al casco urbano" icon={FaMapMarkerAlt} />
-
-                <FormGroup>
-                    <FormLabel>
-                        En caso de que el lugar de la actividad, función o condición esté ubicado en zona rural, ¿cuál es la distancia
-                        aproximada al casco urbano más cercano?
-                    </FormLabel>
-                    <FormControl
-                        type="number"
-                        name="actividadDistancia"
-                        value={formData.actividadDistancia}
-                        onChange={handleChange}
-                        placeholder="Distancia en km"
-                    />
-                </FormGroup>
-            </Col>
-        </Row>
-    </>
+        <FormGroup>
+          <FormLabel>Fecha</FormLabel>
+          <FormControl
+            type="date"
+            name="inspeccionFechaActividad"
+            value={formData.inspeccionFechaActividad}
+            onChange={handleChange}
+          />
+        </FormGroup>
+      </Col>
+      <Col md={6} className="mb-3">
+        <SubtituloForm
+          subtitulo="Distancia al casco urbano"
+          icon={FaMapMarkerAlt}
+        />
+        <Form.Group controlId={`residencialDistancia`}>
+          <FormLabel>Distancia</FormLabel>
+          <OverlayTrigger
+            placement="right"
+            overlay={
+              <Tooltip id={`tooltip-pv`}>
+                En caso de que el lugar de la actividad, función o condición
+                esté ubicado en zona rural, ¿cuál es la distancia aproximada al
+                casco urbano más cercano?
+              </Tooltip>
+            }
+          >
+            <Form.Control
+              type="number"
+              name="actividadDistancia"
+              value={formData.actividadDistancia}
+              onChange={handleChange}
+              placeholder="Distancia en km"
+            />
+          </OverlayTrigger>
+        </Form.Group>
+      </Col>
+    </Row>
+  </>
 );
 
 export default ComponenteActividad;
--- src/components/forms/entorno/componenteResidencial.tsx
+++ src/components/forms/entorno/componenteResidencial.tsx
@@ -1,49 +1,74 @@
 import React, { ChangeEvent } from "react";
-import { Row, Col, Form, FormGroup, FormLabel, FormControl } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  FormLabel,
+  FormControl,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { FaCalendarAlt, FaMapMarkerAlt } from "react-icons/fa";
 import { SubtituloForm } from "eco-unp/ui";
 
 interface ComponenteResidencialProps {
-    formData: {
-        inspeccionFechaResidencial: string;
-        residencialDistancia: string;
-    };
-    handleChange: (e: ChangeEvent<any>) => void;
+  formData: {
+    inspeccionFechaResidencial: string;
+    residencialDistancia: string;
+  };
+  handleChange: (e: ChangeEvent<any>) => void;
 }
 
-const ComponenteResidencial: React.FC<ComponenteResidencialProps> = ({ formData, handleChange }) => (
-    <>
-        <Row>
-            <Col md={6} className="mb-3">
-            <SubtituloForm subtitulo="Fecha de realización de la inspección" icon={FaCalendarAlt} />
-                <FormGroup>
-                    <FormLabel>Fecha</FormLabel>
-                    <FormControl
-                        type="date"
-                        name="inspeccionFechaResidencial"
-                        value={formData.inspeccionFechaResidencial}
-                        onChange={handleChange}
-                    />
-                </FormGroup>
-            </Col>
-            <Col md={6} className="mb-3">
-            <SubtituloForm subtitulo="Distancia al casco urbano" icon={FaMapMarkerAlt} />
-                <FormGroup>
-                    <FormLabel>
-                        En caso de que la vivienda esté ubicada en zona rural, ¿cuál es la distancia aproximada al casco urbano más
-                        cercano?
-                    </FormLabel>
-                    <FormControl
-                        type="number"
-                        name="residencialDistancia"
-                        value={formData.residencialDistancia}
-                        onChange={handleChange}
-                        placeholder="Distancia en km"
-                    />
-                </FormGroup>
-            </Col>
-        </Row>
-    </>
+const ComponenteResidencial: React.FC<ComponenteResidencialProps> = ({
+  formData,
+  handleChange,
+}) => (
+  <>
+    <Row>
+      <Col md={6} className="mb-3">
+        <SubtituloForm
+          subtitulo="Fecha de realización de la inspección"
+          icon={FaCalendarAlt}
+        />
+        <FormGroup>
+          <FormLabel>Fecha</FormLabel>
+          <FormControl
+            type="date"
+            name="inspeccionFechaResidencial"
+            value={formData.inspeccionFechaResidencial}
+            onChange={handleChange}
+          />
+        </FormGroup>
+      </Col>
+      <Col md={6} className="mb-3">
+        <SubtituloForm
+          subtitulo="Distancia al casco urbano"
+          icon={FaMapMarkerAlt}
+        />
+        <Form.Group controlId={`residencialDistancia`}>
+        <FormLabel>Distancia</FormLabel>  
+          <OverlayTrigger
+            placement="right"
+            overlay={
+              <Tooltip id={`tooltip-pv`}>
+                En caso de que la vivienda esté ubicada en zona rural, ¿cuál es
+                la distancia aproximada al casco urbano más cercano?
+              </Tooltip>
+            }
+          >
+            <Form.Control
+              type="number"
+              name="residencialDistancia"
+              value={formData.residencialDistancia}
+              onChange={handleChange}
+              placeholder="Distancia en km"
+            />
+          </OverlayTrigger>
+        </Form.Group>
+      </Col>
+    </Row>
+  </>
 );
 
 export default ComponenteResidencial;
